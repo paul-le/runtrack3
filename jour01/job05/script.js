b9c099c88379169f999a1dc1e4c5064d2495b664
paul-le <paulloukyle@gmail.com>
2020-03-09 11:23:28 +0100

    Push job05

---
+++ jour01/job05/script.js
@@ -0,0 +1,10 @@
+
+    function afficherjourssemaines()
+    {
+        var jourssemaines = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+
+        for(var i = 0; i != 7 ; i++)
+        {
+            console.log(jourssemaines[i]);
+        }
+    }
