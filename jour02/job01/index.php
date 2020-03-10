d97916095a5246ac9571e537b087aadf97816eac
paul-le <paulloukyle@gmail.com>
2020-03-10 10:09:38 +0100

    Push du job01

---
+++ jour02/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="script.js"></script>
+</head>
+<body>
+    <article id="citation">
+        "La vie a beaucoup plus d’imagination que nous."
+    </article>
+    <button onclick="citation()" class="button">Bouton</button>
+</body>
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,7 @@
+
+    function citation()
+    {
+        var toast = document.getElementById("citation");
+        var contenu = toast.textContent;
+        console.log(contenu);
+    }
