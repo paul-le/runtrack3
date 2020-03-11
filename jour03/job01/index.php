c6c815e30ea083c220cf37b8e0e9d70a8fb06c96
paul-le <paulloukyle@gmail.com>
2020-03-11 10:53:43 +0100

    Push du job01 jour 3

---
+++ jour03/job01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html id="yaplusrien" lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <button id="button">PHP > JavaScript</button>
+    <article id="blabla" class="invisible">
+        Le $ est un bon élément
+        monétaire. Le # de twitter est une bonne I D. Il faudra faire le point avec la
+        classe pour cacher cet élément.
+    </article><br><br>
+    <button id="bye">Au revoir</button>
+    <script src="jQuery.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ jour03/job01/script.js
@@ -0,0 +1,9 @@
+
+
+    $("#button").click(function(){
+        $("#blabla").css("display","initial");
+    });
+
+    $("#bye").click(function(){
+        $("#yaplusrien").css("display","none");
+    });
